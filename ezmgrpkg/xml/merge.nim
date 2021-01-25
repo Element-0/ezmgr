e56a056d3e45b7712f856dd121c3ba29a03966a7
CodeHz <codehz@outlook.com>
2021-01-25 14:48:43 +0800

    fix merge repo

--- ezmgrpkg/xml/merge.nim
+++ ezmgrpkg/xml/merge.nim
@@ -1,4 +1,5 @@
 import xmlio, vtable, tables, base, ns, uri
+import ezcommon/version_code
 
 declareXmlElement:
   type MergeRepository* {.id: "df4cd0df-ce37-457f-a3f8-ac5d38742b21", children: repos.} = object of RootObj
@@ -8,7 +9,11 @@ impl MergeRepository, ModRepository:
   method list*(self: ref MergeRepository, base: Uri): Table[string, ref ModInfo] =
     for repo in self.repos:
       for key, value in repo.list(base):
-        result[key] = value
+        result.withValue(key, tmp):
+          if tmp[].desc().version < value.desc().version:
+            tmp[] = value
+        do:
+          result[key] = value
 
 rootns.registerType("merge-repo", ref MergeRepository, ref ModRepository)
 
