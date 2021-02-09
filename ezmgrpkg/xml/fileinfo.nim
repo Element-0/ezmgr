bdf637646af3a0396b9999dcc5b5321c2eecbcda
CodeHz <codehz@outlook.com>
2021-02-10 05:41:15 +0800

    use VarFileInfo

--- ezmgrpkg/xml/fileinfo.nim
+++ ezmgrpkg/xml/fileinfo.nim
@@ -8,17 +8,17 @@ type LANGANDCODEPAGE {.pure, final.} = object
 
 type InvalidFileInfoError* = object of ValueError
 
-template queryKind(x: static string): string =
+template queryKind(x: static string, isvar: static bool = false): string =
   var desc: LPWSTR
   var desc_n: UINT
-  let path = basepart & x
+  let path = (when isvar: varpart else: basepart) & x
   if VerQueryValue(pblock, path, cast[ptr PVOID](addr desc), addr desc_n) == 0:
     raise newException(InvalidFileInfoError, x & " not found in file")
   $desc
-template queryOptionalKind(x: static string): string =
+template queryOptionalKind(x: static string, isvar: static bool = false): string =
   var desc: LPWSTR
   var desc_n: UINT
-  let path = basepart & x
+  let path = (when isvar: varpart else: basepart) & x
   if VerQueryValue(pblock, path, cast[ptr PVOID](addr desc), addr desc_n) == 0:
     ""
   else:
@@ -34,24 +34,25 @@ proc parseModFile*(str: string): tuple[id: string, desc: ref ModDescription] =
   if GetFileVersionInfo(str, 0, size, pblock) == 0:
     raise newException(InvalidFileInfoError, "failed to read file version")
   var fixed: ptr VS_FIXEDFILEINFO
-  if VerQueryValue(pblock, L"\\", cast[ptr PVOID](addr fixed), nil) == 0:
+  if VerQueryValue(pblock, L"\", cast[ptr PVOID](addr fixed), nil) == 0:
     raise newException(InvalidFileInfoError, "no version information")
   result.desc.version = VersionCode((uint64(fixed[].dwFileVersionMS) shl 32) or uint64(fixed[].dwFileVersionLS))
   if result.desc.version == VersionCode(0):
     raise newException(InvalidFileInfoError, "zero version code found")
   var langcode: ptr LANGANDCODEPAGE
   var langcode_n: UINT
-  if VerQueryValue(pblock, L"\\VarFileInfo\\Translation", cast[ptr PVOID](addr langcode), addr langcode_n) == 0:
+  if VerQueryValue(pblock, L"\VarFileInfo\Translation", cast[ptr PVOID](addr langcode), addr langcode_n) == 0:
     raise newException(InvalidFileInfoError, "no version translation found")
   if langcode_n == 0 or langcode_n mod sizeof(LANGANDCODEPAGE) != 0:
     raise newException(InvalidFileInfoError, "invalid version translation count")
   let lc = langcode[]
   let basepart = fmt"\StringFileInfo\{lc.lang:04x}{lc.code:04x}\"
+  let varpart = r"\VarFileInfo\"
   result.id = queryKind("InternalName")
-  result.desc.kind = parseEnum(queryKind("ModKind"), modServerSide)
+  result.desc.kind = parseEnum(queryKind("ModKind", true), modServerSide)
   result.desc.name = queryKind("OriginalFilename")
   result.desc.desc = queryKind("FileDescription")
   result.desc.author = queryKind("CompanyName")
   result.desc.comment = queryOptionalKind("Comments")
-  result.desc.license = queryKind("Licence")
+  result.desc.license = queryKind("Licence", true)
   # TODO: load depends info
